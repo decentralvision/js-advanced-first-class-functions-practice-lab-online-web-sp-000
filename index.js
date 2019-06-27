1c5a5b74ea601eb766d084f806632b396573fda9
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 02:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 * `logDriverNames()` — Receives an array of `driver` objects and logs the `name` attribute of each `driver` to the console.
-const logDriverNames = (function(drivers) {})
+const logDriverNames = (function(drivers) {drivers.forEach{}})
 * `logDriversByHometown()` — Receives an array of `driver` objects as the first argument and a location as the second argument. The function logs to the console the `name` attribute of each `driver` whose hometown matches the string passed in as the 'location' argument.
 * `driversByRevenue()` — Receives an array of `driver` objects and returns a new array of `driver` objects sorted by their `revenue` attribute from lowest to highest.
 * `driversByName()` — Receives an array of `driver` objects and returns a new array of `driver` objects sorted by their `name` attribute from A to Z. Here, you may have to use the [`String.prototype.localeCompare()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare) method.
